302f2da2494c466d7fb678894171c606db16eec7
Guilherme Freitas <gv.freitas2003@usp.br>
2023-11-25 18:29:56 -0300

    R squared in sample

--- Code/R/RegreOS.R
+++ Code/R/RegreOS.R
@@ -1,31 +1,40 @@
-
 # Regression
 
 library(readxl)
 library(skedastic)
 
-setwd("C:/Users/Leandro/Desktop/Livre-Docente/Codigos/sp500analysis")
-sp_tech <- read_excel("sp_tech.xlsx")
-
+setwd("C:/Users/User/Downloads")
+data <- read_excel("data.xlsx")
+# data1 <- read_excel("data.xlsx")
 # --------------------
 
-total <- nrow(sp_tech)
-insample <- 181 # until dec 1965 like Neely
+total <- nrow(data)
+insample <- 70
 forecasts <- total - insample - 1
 
 resultados <- matrix(0,nrow = forecasts,ncol = 16)
-j<-7
+j<-3
 for(h in 1:14){
 for(i in 1:forecasts){
-regressao <- lm(sp_tech$premium[2:(i+1+insample-1)]~as.matrix(sp_tech[1:(i+insample-1),j]))
-forecast <- as.numeric(regressao[["coefficients"]][1]) + as.numeric(regressao[["coefficients"]][1])*as.numeric(sp_tech[i+1+insample-1,j])
-real <- sp_tech$premium[(i+2+insample-1)]
+regressao <- lm(data$prem[2:(i+1+insample-1)]~as.matrix(data[1:(i+insample-1),j]))
+forecast <- as.numeric(regressao[["coefficients"]][1]) + as.numeric(regressao[["coefficients"]][1])*as.numeric(data[i+1+insample-1,j])
+real <- data$prem[(i+2+insample-1)]
 resultados[i,h+1] <- forecast
 resultados[i,1] <- real
 }
   j <- j+1
 }
-resultados[,16] <- sp_tech$rHA[(insample+2):total]
+resultados[,16] <- data$HA[(insample+1):(total-1)]
 
+rquadrados = matrix(0,nrow = 14,ncol = 4)
+for(i in 1:14){
+  
+  rquadrados[i,1]= 1-(sum((((resultados[,1]-resultados[,i+1])^2)))/sum(((resultados[,1]-resultados[,16])^2)))
+  rquadrados[i,2]= 1-(sum((((resultados[,1]-resultados[,i+1])^2)*data$Fear[(insample+1):276]))/sum(((resultados[,1]-resultados[,16])^2)*data$Fear[(insample+1):276]))
+  rquadrados[i,3]= 1-(sum((((resultados[,1]-resultados[,i+1])^2)*data$Greed[(insample+1):276]))/sum(((resultados[,1]-resultados[,16])^2)*data$Greed[(insample+1):276]))
+  rquadrados[i,4]= 1-(sum((((resultados[,1]-resultados[,i+1])^2)*data$Neutral[(insample+1):276]))/sum(((resultados[,1]-resultados[,16])^2)*data$Neutral[(insample+1):276]))
+  
+}
+rquadrados = rquadrados*100
 
-
+write.csv(rquadrados, 'r2_OS_normal.csv')
